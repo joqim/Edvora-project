8f59d2269f989e74dc79e08af6bf781b116de83c
Joqim <dhipauk27@gmail.com>
2022-01-04 12:23:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+
+# Use this to serve a public/index.html
+from starlette.responses import FileResponse 
 import requests
 
 from pymongo import MongoClient
@@ -20,7 +22,7 @@ test_user = {
     "password": "bat1",
 }
 
-app = FastAPI()
+app = FastAPI(static_folder='my-app/build', static_url_path='')
 
 origins = {
     "http://localhost",
@@ -36,65 +38,70 @@ app.add_middleware(
     allow_headers= ["*"],
 )
 
-
-app = FastAPI()
-app.mount('/', StaticFiles(directory="./my-app/build/static", html=True), name="static")
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
-templates = Jinja2Templates(directory="templates")
-
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient("mongodb+srv://cs631:edvora1998@cluster0.sug2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db=client.edvora
-user = {
-        'name' : 'joqim',
-        'favourite_pokemon' : 'bulbasaur'
+@app.route('/api', methods=['GET'])
+def index():
+    return {
+        "tutorial": "Flask React Heroku"
     }
-result=db.users.insert_one(user)
 
-class LoginItem(BaseModel):
-    username: str
-    password: str
+app.mount("/my-app/public", StaticFiles(directory="./my-app/public"), name="public")
+
+@app.get("/")
+async def read_index():
+    return FileResponse('public/index.html')
+
+# # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+# client = MongoClient("mongodb+srv://cs631:edvora1998@cluster0.sug2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# db=client.edvora
+# user = {
+#         'name' : 'joqim',
+#         'favourite_pokemon' : 'bulbasaur'
+#     }
+# result=db.users.insert_one(user)
+
+# class LoginItem(BaseModel):
+#     username: str
+#     password: str
     
-    @app.get("/")
-    def read_root():
-     return {"Hello": "World"}
+#     @app.get("/")
+#     def read_root():
+#      return {"Hello": "World"}
 
-@app.post("/login")
-async def user_login(loginitem:LoginItem):
-    data = jsonable_encoder(loginitem)
-    if data['username']== test_user['username'] and data['password']== test_user['password']:
-        print ("The login values match")
-        encoded_jwt = jwt.encode(data, SECERT_KEY, algorithm=ALGORITHM)
-        return {'token': encoded_jwt}        
-    else:
-        return {'message':'login failed'}
+# @app.post("/login")
+# async def user_login(loginitem:LoginItem):
+#     data = jsonable_encoder(loginitem)
+#     if data['username']== test_user['username'] and data['password']== test_user['password']:
+#         print ("The login values match")
+#         encoded_jwt = jwt.encode(data, SECERT_KEY, algorithm=ALGORITHM)
+#         return {'token': encoded_jwt}        
+#     else:
+#         return {'message':'login failed'}
 
 
 
-class PokeItem(BaseModel):
-    pokemon_name: str
+# class PokeItem(BaseModel):
+#     pokemon_name: str
 
-    @app.get("/")
-    def read_root():
-     return {"Hello": "World"}
+#     @app.get("/")
+#     def read_root():
+#      return {"Hello": "World"}
 
-@app.post("/save_pokemon")
-async def user_login(pokemon:PokeItem):
-    data = jsonable_encoder(pokemon)
-    print (data['pokemon_name'])
-    if data['pokemon_name']:
-        print ("Pokemon name is given", data['pokemon_name'])
+# @app.post("/save_pokemon")
+# async def user_login(pokemon:PokeItem):
+#     data = jsonable_encoder(pokemon)
+#     print (data['pokemon_name'])
+#     if data['pokemon_name']:
+#         print ("Pokemon name is given", data['pokemon_name'])
 
-        #convert input name to lowercase
-        pokemon_name = data['pokemon_name']
-        base_url = 'https://pokeapi.co/api/v2/pokemon'
-        pokemon_response = requests.get(f'{base_url}/{pokemon_name}')
-        # pokemon_response = requests.get(f'{base_url}/clefairy')
-        print(pokemon_response)
-        return {'token': "pokemon saved in Database"}        
-    else:
-        return {'message':'login failed'}
+#         #convert input name to lowercase
+#         pokemon_name = data['pokemon_name']
+#         base_url = 'https://pokeapi.co/api/v2/pokemon'
+#         pokemon_response = requests.get(f'{base_url}/{pokemon_name}')
+#         # pokemon_response = requests.get(f'{base_url}/clefairy')
+#         print(pokemon_response)
+#         return {'token': "pokemon saved in Database"}        
+#     else:
+#         return {'message':'login failed'}
 
 # @app.post("/save_pokemon")
 #     print ("inside save pokemon")
