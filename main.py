b0d76145b58041611c70526fa601fa2ae3e3f4b6
Joqim <dhipauk27@gmail.com>
2022-01-04 19:42:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -36,24 +36,24 @@ folder = 'my-app/build/'
 
 app.mount("/static/", StaticFiles(directory="my-app/build/static"), name="static")
 
-@app.get("/", response_class=FileResponse)
-def read_index(request: Request):
-    path = 'my-app/build/index.html'
-    return FileResponse(path)
-
-# @app.get("/{catchall:path}", response_class=FileResponse)
+# @app.get("/", response_class=FileResponse)
 # def read_index(request: Request):
-#     # check first if requested file exists
-#     path = request.path_params["catchall"]
-#     file = folder+path
+#     path = 'my-app/build/index.html'
+#     return FileResponse(path)
 
-#     print('look for: ', path, file)
-#     if os.path.exists(file):
-#         return FileResponse(file)
+@app.get("/{catchall:path}", response_class=FileResponse)
+def read_index(request: Request):
+    # check first if requested file exists
+    path = request.path_params["catchall"]
+    file = folder+path
 
-#     # otherwise return index files
-#     index = 'my-app/build/index.html' 
-#     return FileResponse(index)
+    print('look for: ', path, file)
+    if os.path.exists(file):
+        return FileResponse(file)
+
+    # otherwise return index files
+    index = 'my-app/build/index.html' 
+    return FileResponse(index)
 
 #put in env, mongoURI string
 client = MongoClient("mongodb+srv://cs631:edvora1998@cluster0.sug2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
