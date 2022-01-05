45bcc6c19b790e4679ea0c9101615681f45a80c8
Joqim <dhipauk27@gmail.com>
2022-01-04 19:37:42 -0800

    testing without serving

--- main.py
+++ main.py
@@ -36,24 +36,24 @@ folder = 'my-app/build/'
 
 app.mount("/static/", StaticFiles(directory="my-app/build/static"), name="static")
 
-@app.get("/", response_class=FileResponse)
-def read_index(request: Request):
-    path = 'my-app/build/index.html'
-    return FileResponse(path)
+# @app.get("/", response_class=FileResponse)
+# def read_index(request: Request):
+#     path = 'my-app/build/index.html'
+#     return FileResponse(path)
 
-@app.get("/{catchall:path}", response_class=FileResponse)
-def read_index(request: Request):
-    # check first if requested file exists
-    path = request.path_params["catchall"]
-    file = folder+path
+# @app.get("/{catchall:path}", response_class=FileResponse)
+# def read_index(request: Request):
+#     # check first if requested file exists
+#     path = request.path_params["catchall"]
+#     file = folder+path
 
-    print('look for: ', path, file)
-    if os.path.exists(file):
-        return FileResponse(file)
+#     print('look for: ', path, file)
+#     if os.path.exists(file):
+#         return FileResponse(file)
 
-    # otherwise return index files
-    index = 'my-app/build/index.html' 
-    return FileResponse(index)
+#     # otherwise return index files
+#     index = 'my-app/build/index.html' 
+#     return FileResponse(index)
 
 #put in env, mongoURI string
 client = MongoClient("mongodb+srv://cs631:edvora1998@cluster0.sug2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
